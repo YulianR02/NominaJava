f1478b3002160b60c273f01dc9a62f0624411e23
Deivy Yulian Ruiz Gutierrez <dyruiz603@misena.edu.co>
2021-04-29 23:34:46 -0500

    Segunda version

--- src/main/java/com/nomina/nominaJava/HelloServlet.java
+++ src/main/java/com/nomina/nominaJava/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.nomina.nominaJava;
 
 import java.io.*;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,34 +10,41 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-    private String nom;
-
-    public void init() {
-        message = "Hello World!";
-    }
 
 
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("<h1>Nombres:</h1>");
-        String nom=request.getParameter("nombres");
-        out.println(nom);
-        out.println("<br>");
-        out.println("Apellidos:");
-        String ape=request.getParameter("apellidos");
-        out.println(ape);
+        out.println("<h1><center>Nominas</center></h1>");
+        out.print("<h3>Nombre:</h3>");
+        String nombre=request.getParameter("nombres");
+        out.print(nombre);
+        out.print("<h3>Apellidos:</h3>");
+        String apellido=request.getParameter("apellidos");
+        out.print(apellido);
+        out.print("<h3>Documento:</h3>");
+        String documento=request.getParameter("documento");
+        out.print(documento);
+        out.print("<h3>Dias Trabajados:</h3>");
+        String diaTrabajados=request.getParameter("diasTrabajados");
+        out.print(diaTrabajados);
+        out.print("<h3>Sueldo:</h3>");
+        String sueldo=request.getParameter("sueldoMensual");
+        out.print(sueldo);
+
+
 
         out.println("</body></html>");
 
 
     }
+
+
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     }
